a5392230a63cd8c52b33b78b49abb54b4b13e268
Ngannguyen0308 <Ngannguyen0308@gmail.com>
2021-12-02 15:40:21 +0700

    add file

---
+++ SQLHMS.sql
@@ -0,0 +1,31 @@
+create table rooms(
+roomid int Identity(1,1) primary key,
+roomNo varchar(250) not null unique,
+roomType varchar(250) not null,
+bed varchar(250) not null,
+price bigint not null,
+booked varchar(50) default 'NO'
+);
+
+select * from rooms
+roomNo,roomType,bed,price
+
+create table customer(
+cid int Identity(1,1) primary key,
+cname varchar(250) not null,
+mobile bigint not null,
+nationality varchar(250) not null,
+gender varchar(50) not null,
+dob varchar(50) not null,
+idcard varchar(250) not null,
+addres varchar(350) not null,
+checkin varchar(250) not null,
+checkout varchar(250),
+chekout varchar(250) not null default 'NO',
+roomid int not null,
+foreign key (roomid) references rooms(roomid)
+);
+
+select * from customer
+
+drop database myHotel
